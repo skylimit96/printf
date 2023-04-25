bc386e9ef3d896d3ac2ca801e37d17d5b94f5637
EssaadiZaynab <125550252+EssaadiZaynab@users.noreply.github.com>
2023-04-25 21:11:10 +0000

    Update utils.c

--- utils.c
+++ utils.c
@@ -76,7 +76,7 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
-long int convert_size_unsgnd(unsigned long int num, int size)
+long int  convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
